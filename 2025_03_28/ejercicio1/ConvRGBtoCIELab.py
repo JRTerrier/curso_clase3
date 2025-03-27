6e524a4a8a240517f610f01fe3c9e1fd59c7fe69
JRTerrier <66105609+JRTerrier@users.noreply.github.com>
2025-03-27 14:58:01 -0600

    Add files via upload

---
+++ 2025_03_28/ejercicio1/ConvRGBtoCIELab.py
@@ -0,0 +1,92 @@
+from PIL import ImageCms
+
+
+class ColorTrans:
+
+    '''Class for transforming RGB<->LAB color spaces for PIL images.'''
+
+    def __init__(self,  img):
+        self.img=img
+        self.srgb_p = ImageCms.createProfile("sRGB")
+        self.lab_p  = ImageCms.createProfile("LAB")
+        self.rgb2lab_trans = ImageCms.buildTransformFromOpenProfiles(self.srgb_p, self.lab_p, "RGB", "LAB")
+        self.lab2rgb_trans = ImageCms.buildTransformFromOpenProfiles(self.lab_p, self.srgb_p, "LAB", "RGB")
+
+    def rgb2lab(self):
+        return ImageCms.applyTransform(self.img, self.rgb2lab_trans)
+
+    def lab2rgb(self, img):
+        return ImageCms.applyTransform(self.img, self.lab2rgb_trans)
+
+def rgb2lab ( inputColor ) :
+
+   num = 0
+   RGB = [0, 0, 0]
+
+   for value in inputColor :
+       value = float(value) / 255
+
+       if value > 0.04045 :
+           value = ( ( value + 0.055 ) / 1.055 ) ** 2.4
+       else :
+           value = value / 12.92
+
+       RGB[num] = value * 100
+       num = num + 1
+
+   XYZ = [0, 0, 0,]
+
+   X = RGB [0] * 0.4124 + RGB [1] * 0.3576 + RGB [2] * 0.1805
+   Y = RGB [0] * 0.2126 + RGB [1] * 0.7152 + RGB [2] * 0.0722
+   Z = RGB [0] * 0.0193 + RGB [1] * 0.1192 + RGB [2] * 0.9505
+   XYZ[ 0 ] = round( X, 4 )
+   XYZ[ 1 ] = round( Y, 4 )
+   XYZ[ 2 ] = round( Z, 4 )
+
+   XYZ[ 0 ] = float( XYZ[ 0 ] ) / 95.047         # ref_X =  95.047   Observer= 2°, Illuminant= D65
+   XYZ[ 1 ] = float( XYZ[ 1 ] ) / 100.0          # ref_Y = 100.000
+   XYZ[ 2 ] = float( XYZ[ 2 ] ) / 108.883        # ref_Z = 108.883
+
+   num = 0
+   for value in XYZ :
+
+       if value > 0.008856 :
+           value = value ** ( 0.3333333333333333 )
+       else :
+           value = ( 7.787 * value ) + ( 16 / 116 )
+
+       XYZ[num] = value
+       num = num + 1
+
+   Lab = [0, 0, 0]
+
+   L = ( 116 * XYZ[ 1 ] ) - 16
+   a = 500 * ( XYZ[ 0 ] - XYZ[ 1 ] )
+   b = 200 * ( XYZ[ 1 ] - XYZ[ 2 ] )
+
+   Lab [ 0 ] = round( L, 4 )
+   Lab [ 1 ] = round( a, 4 )
+   Lab [ 2 ] = round( b, 4 )
+
+   return Lab
+
+
+def rgb_to_hsv(r, g, b):
+    r, g, b = r/255.0, g/255.0, b/255.0
+    mx = max(r, g, b)
+    mn = min(r, g, b)
+    df = mx-mn
+    if mx == mn:
+        h = 0
+    elif mx == r:
+        h = (60 * ((g-b)/df) + 360) % 360
+    elif mx == g:
+        h = (60 * ((b-r)/df) + 120) % 360
+    elif mx == b:
+        h = (60 * ((r-g)/df) + 240) % 360
+    if mx == 0:
+        s = 0
+    else:
+        s = (df/mx)*100
+    v = mx*100
+    return h, s, v
